025113eea0ad8678fb9afc0b76b4a45af1df3ef2
Miklós Galicz <blackfyre@users.noreply.github.com>
2024-10-09 10:59:56 +0200

    chore: :hammer: add goreleaser to packages in devenv.nix file

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,10 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [ 
+    pkgs.git
+    pkgs.goreleaser
+  ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
